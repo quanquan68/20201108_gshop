b033444f6256e92ef2f8690c73134d418a73c472
圈圈 <wangjiayuan@wangjiayuandeMacBook-Air.local>
2020-11-08 20:27:12 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+/* eslint-disable */
+<template>
+    <div id="app">
+    <router-view/><!--是上面的路由部分-->
+      <FooterGuide v-show="$route.meta.showFooter"/><!--下面的底部导航部分,用v-show来实现是否显示-->
+    </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+export default {
+    components:{
+      FooterGuide
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app {
+    width: 100%;
+    height: 100%;
+    background: #f5f5f5;
+    position: relative;
+  }
+</style>
